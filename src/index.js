33073ee866a150d313adc4dfbf3b80be89f360a4
DanielJDufour <daniel.j.dufour@gmail.com>
2020-10-30 01:36:00 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* global customElements */
 /* global HTMLElement */
 
-(function() {
+(function () {
   class DynamicGallery extends HTMLElement {
     constructor() {
       // establish prototype chain
@@ -12,7 +12,7 @@
     }
 
     static get observedAttributes() {
-      return ['color', 'data-sources', 'height', 'width'];
+      return ["color", "data-sources", "height", "width"];
     }
 
     // fires after the element has been attached to the DOM
@@ -20,21 +20,87 @@
       this.render();
     }
 
+    get debug() {
+      return this.getAttribute("debug") === "true";
+    }
+
+    get color() {
+      return this.getAttribute("color") || "chartreuse";
+    }
+
+    get count() {
+      return this.querySelectorAll("li").length;
+    }
+
+    add(source) {
+      try {
+        // update data-sources in DOM
+        const sources = JSON.parse(this.getAttribute("data-sources"));
+        sources.push(source);
+        this.setAttribute("data-sources", JSON.stringify(sources));
+
+        // adding image
+        const img = document.createElement("img");
+        img.src = source;
+        img.style.width = "100%";
+        img.style.display = "none";
+        this.querySelector(".carousel").append(img);
+
+        // update pagination
+        const li = document.createElement("li");
+        li.style.cursor = "pointer";
+        li.style.display = "inline-block";
+        li.style.margin = "10px";
+        li.style.padding = "10px";
+        li.style.background = "darkgray";
+        li.style.boxShadow = "5px 5px";
+        li.style.userSelect = "none";
+
+        const i = this.count;
+
+        li.textContent = i + 1;
+
+        li.addEventListener("click", () => {
+          // don't do anything if clicking currently selected
+          if (this.state.page === i) return;
+
+          const imgs = this.querySelectorAll("img");
+
+          // display new image
+          imgs[i].style.display = null;
+
+          // hide old image
+          imgs[this.state.page].style.display = "none";
+
+          // change pagination
+          li.style.background = this.color;
+          this.querySelectorAll("li")[this.state.page].style.background =
+            "darkgray";
+
+          // update page
+          this.state.page = i;
+        });
+
+        this.querySelector("ul").appendChild(li);
+      } catch (error) {
+        console.error("dynamic-gallery failed to add");
+        console.error(error);
+        throw error;
+      }
+    }
+
     render() {
       try {
-        const color = this.getAttribute("color") || "chartreuse";
-
         const sources = JSON.parse(this.getAttribute("data-sources"));
 
         const inner = document.createElement("div");
         inner.style.margin = "0 auto";
         inner.style.width = this.getAttribute("width") || "300px";
 
-
         const carouselWrapper = document.createElement("div");
         carouselWrapper.className = "carousel-wrapper";
         carouselWrapper.style.height = this.getAttribute("height") || "400px";
-        carouselWrapper.style.overflowY = 'auto';
+        carouselWrapper.style.overflowY = "auto";
         carouselWrapper.style.width = "100%";
         const carousel = document.createElement("div");
         carousel.style.width = "100%";
@@ -43,7 +109,7 @@
           const img = document.createElement("img");
           img.src = source;
           img.style.width = "100%";
-          if (i !== this.state.page ) img.style.display = "none";
+          if (i !== this.state.page) img.style.display = "none";
           carousel.append(img);
         });
         carouselWrapper.appendChild(carousel);
@@ -56,12 +122,15 @@
           li.style.display = "inline-block";
           li.style.margin = "10px";
           li.style.padding = "10px";
-          li.style.background = i === this.state.page ? color : "darkgray";
+          li.style.background = i === this.state.page ? this.color : "darkgray";
           li.style.boxShadow = "5px 5px";
           li.style.userSelect = "none";
           li.textContent = i + 1;
           li.addEventListener("click", () => {
-            const imgs = this.querySelectorAll('img');
+            // don't do anything if clicking currently selected
+            if (this.state.page === i) return;
+
+            const imgs = this.querySelectorAll("img");
 
             // display new image
             imgs[i].style.display = null;
@@ -70,8 +139,9 @@
             imgs[this.state.page].style.display = "none";
 
             // change pagination
-            li.style.background = color;
-            this.querySelectorAll('li')[this.state.page].style.background = "darkgray";
+            li.style.background = this.color;
+            this.querySelectorAll("li")[this.state.page].style.background =
+              "darkgray";
 
             // update page
             this.state.page = i;
@@ -83,13 +153,12 @@
         inner.appendChild(pagination);
 
         this.appendChild(inner);
-
       } catch (error) {
         console.error("dynamic-gallery failed to render");
-	console.error(error);
-	throw error;
+        console.error(error);
+        throw error;
       }
     }
   }
-  customElements.define('dynamic-gallery', DynamicGallery);
+  customElements.define("dynamic-gallery", DynamicGallery);
 })();
